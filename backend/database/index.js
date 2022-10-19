6f9e9be0dc59099d41a5c1021d7458c14487d50d
Caranell <lyachek.anton@gmail.com>
2022-10-19 18:08:45 +0400

    Init

---
+++ backend/database/index.js
@@ -0,0 +1,18 @@
+const { MongoClient } = require('mongodb');
+
+const CONNECTION_URL = process.env.CONNECTION_URL || 'mongodb://localhost:27017';
+const DB_NAME = 'Profiles';
+
+let dbConnection;
+const client = new MongoClient(CONNECTION_URL, {
+  useUnifiedTopology: true,
+});
+
+module.exports = {
+  connectToDB: async () => {
+    const connection = await client.connect();
+    dbConnection = connection.db(DB_NAME);
+    return client;
+  },
+  getDbConnection: () => dbConnection,
+};
---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const { connectToDB } = require('./database');
+const profileRouter = require('./routes/profile');
+
+require('dotenv').config();
+
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+await connectToDB();
+
+app.use('/profile', profileRouter);
+
+app.listen(PORT);
