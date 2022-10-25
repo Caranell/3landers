7881432828b4444838c7768adccc4a61479b7428
Caranell <lyachek.anton@gmail.com>
2022-10-25 23:43:51 +0400

    Add auth routes

---
+++ backend/controllers/auth.js
@@ -0,0 +1,43 @@
+const { Router } = require('express');
+const { generateNonce, SiweMessage } = require('siwe');
+
+const router = Router();
+
+router.get('/nonce', async (req, res) => {
+  req.session.nonce = generateNonce();
+  await req.session.save();
+  res.setHeader('Content-Type', 'text/plain');
+
+  res.send(req.session.nonce);
+});
+
+router.post('/verify', async (req, res) => {
+  try {
+    const { message, signature } = req.body;
+    const siweMessage = new SiweMessage(message);
+    const fields = await siweMessage.validate(signature);
+
+    if (fields.nonce !== req.session.nonce) {
+      return res.status(422).json({ message: 'Invalid nonce.' });
+    }
+
+    req.session.siwe = fields;
+    await req.session.save();
+
+    return res.status(200).json({ ok: true });
+  } catch (_error) {
+    return res.status(400).json({ ok: false });
+  }
+});
+
+router.get('/session', async (req, res) => {
+  res.send({ address: req.session.siwe?.address });
+});
+
+router.post('/sign-out', async (req, res) => {
+  req.session.destroy();
+
+  res.sendStatus(200);
+});
+
+module.exports = router;
--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,29 @@
 const express = require('express');
 const cors = require('cors');
+const session = require('express-session');
 const { connectToDB } = require('./database');
 const profileRouter = require('./controllers/profile');
+const authRouter = require('./controllers/auth');
 
 require('dotenv').config();
 
 const PORT = process.env.PORT || 8000;
+const SECRET = process.env.SECRET || 'secret';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(
+  session({
+    secret: SECRET,
+    cookie: { maxAge: 60000 },
+  }),
+);
 
 connectToDB();
 
 app.use('/profile', profileRouter);
+app.use('/auth', authRouter);
 
 app.listen(PORT);
